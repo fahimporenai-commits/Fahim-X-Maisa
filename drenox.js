d7467bacb69fd022966396c0c87aedb53b5c6048
fahimporenai-commits <fahimporenai@gmail.com>
2026-09-01 16:04:11 +0600

    Update drenox.js

--- drenox.js
+++ drenox.js
@@ -6829,12 +6829,12 @@ case 'contact': {
     const vcard1 = 'BEGIN:VCARD\n' +
                   'VERSION:3.0\n' +
                   'FN: 𝑺𝑯𝑨𝑫𝑶𝑾︎\n' +
-                  'TEL;type=CELL;type=VOICE;waid=923271054080:+923271054080\n' +
+                  'TEL;type=CELL;type=VOICE;waid=8801843113523:+8801843113523\n' +
                   'END:VCARD';
     
     await bad.sendMessage(m.chat, {
         contacts: {
-            displayName: '𝑺𝒉𝒂𝒅𝒐𝒘☠︎︎',
+            displayName: '𝘍𝘢𝘩𝘪𝘮 𝘔𝘥',
             contacts: [{ vcard: vcard1 }]
         }
     }, { quoted: msg });
@@ -6845,12 +6845,12 @@ case 'contact': {
     const vcard2 = 'BEGIN:VCARD\n' +
                   'VERSION:3.0\n' +
                   'FN: RIZWAN\n' +
-                  'TEL;type=CELL;type=VOICE;waid=923271054080:+923271054080\n' +
+                  'TEL;type=CELL;type=VOICE;waid=8801843113523:+88018433523\n' +
                   'END:VCARD';
     
     await bad.sendMessage(m.chat, {
         contacts: {
-            displayName: 'Shadow',
+            displayName: '𝘍𝘢𝘩𝘪𝘮',
             contacts: [{ vcard: vcard2 }]
         }
     }, { quoted: msg });
